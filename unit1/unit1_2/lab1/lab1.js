a15347e1bd58918ec158822d5ee3e8eed0bd5a0f
Marco Parello <155767698+marcoparello@users.noreply.github.com>
2026-02-06 11:52:08 -0500

    Update lab1.js
    Finished Lab 1

--- unit1/unit1_2/lab1/lab1.js
+++ unit1/unit1_2/lab1/lab1.js
@@ -1,13 +1,40 @@
 // 1. Returns true if the number is even
-function isEven(n) {}
+function isEven(n) {
+    if (n%2 == 0 && typeof n == "number")
+        return true;
+    else
+        return false;
+}
 
 // 2. Returns the sum of all numbers in an array
-function sumArray(arr) {}
+function sumArray(arr) {
+    let sum = 0;
+    for (let i =0; i<arr.length; i++)
+        if (typeof arr[i] == "number")
+            sum += arr[i];
+        else
+            return NaN
+    return sum;
+        
+}
 
 // 3. Receives a user object and returns the email
 // Example: {name:"ana", email:"a@gmu.edu"}
-function getEmail(user) {}
+function getEmail(user) {
+    if (typeof user == "object" && user.name!= undefined && user.email!= undefined )
+        return user.email;
+    else
+        return false
+}
 
 // 4. Returns true if user.role === "admin"
-function isAdmin(user) {}
+function isAdmin(user) {
+    if (typeof user == "object" && user.role == "admin")
+        return true;
+    else
+        return false;
+}
 
+console.log(isEven(4));
+console.log(sumArray([1,2,34,"a"]));
+console.log(getEmail({email:"a"}));
